4c16e1442ef51c63e78804b421ae02bf47c05403
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2025-03-05 01:56:00 +0530

    Pratistha commit

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -5,10 +5,11 @@ from .views import USAJobsView
 urlpatterns= [
     path('',views.index),
     path('convert-to-cv/', views.convert_to_cv, name='convert_to_cv'),  # Route for resume upload and suggestions 
-    path('api/jobs/', USAJobsView.as_view(), name='job-list'), 
-    path('fetch-internships/', views.fetch_internships_view, name='fetch_internships'),
-    path('fetch-jobs/',views.fetch_jobs_view,name='fetch_jobs'),
-    path("fetch-ijobs/", views.job_search_view, name="fetch_ijobs"),
+    # path('api/jobs/', USAJobsView.as_view(), name='job-list'), 
+    # path('fetch-internships/', views.fetch_internships_view, name='fetch_internships'),
+    # path('fetch-jobs/',views.fetch_jobs_view,name='fetch_jobs'),
+    # path("fetch-ijobs/", views.job_search_view, name="fetch_ijobs"),
+    path('search/', views.merged_view, name='search_jobs'),
     #KaustubhSen
     path('/upload/', views.upload_resume, name='upload'),
     path('register/', views.register, name='register'),
